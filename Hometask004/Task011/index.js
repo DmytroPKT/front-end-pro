789fe96351c3ba91fedab16226ba53d69fa689d7
Dmytro Pokotylo <demetriusz.pk@gmail.com>
2023-06-23 17:45:18 +0300

    Task11 completed. Task10 - grammatical mistakes rectified

--- Hometask004/Task011/index.js
+++ Hometask004/Task011/index.js
@@ -1,15 +1,16 @@
 const inputNumber = Number(prompt("Enter number: "))
 let pairedDivisors = []
 let i = 1
+let sum = 0
 
 while(i <= inputNumber) {
     if (inputNumber % i === 0){
         if (i % 2 === 0) {
             pairedDivisors.push(i)
+            sum = sum + i
         }
     }
     i++
 }
-console.log(pairedDivisors.join(', '))
-console.log(`Quantity of paired divisors for ${inputNumber} -    ${pairedDivisors.length}.`)
-alert(`Quantity of paired divisors for ${inputNumber} -    ${pairedDivisors.length}.`)
+console.log(`Summ of paired divisors for ${inputNumber} (${pairedDivisors.join(', ')}) = ${sum}`)
+alert(`Summ of paired divisors for ${inputNumber} (${pairedDivisors.join(', ')}) = ${sum}`)
